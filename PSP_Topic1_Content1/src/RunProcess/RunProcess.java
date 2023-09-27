df605ed9a1c1e9827eee82c2a047d3b7b3d652b5
Esteban Albarrán <77785532+Shylex96@users.noreply.github.com>
2023-09-27 09:19:20 +0200

    Add files via upload

---
+++ PSP_Topic1_Content1/src/RunProcess/RunProcess.java
@@ -0,0 +1,35 @@
+package RunProcess;
+
+import java.io.IOException;
+import java.util.Arrays;
+public class RunProcess
+{
+	public static void main(String[] args) throws IOException
+	{
+		if (args.length <= 0)
+		{
+			System.err.println("Se necesita un programa a ejecutar");
+			System.exit(-1);
+		}
+		ProcessBuilder pb = new ProcessBuilder(args);
+		try
+		{
+			System.out.println("Ejecutando " + Arrays.toString(args));
+			Process process = pb.start();
+			// Esperamos que termine el proceso
+			int retorno = process.waitFor();
+			System.out.println("La ejecución de " + Arrays.toString(args) +
+					" devuelve " + retorno);
+		}
+		catch (IOException ex)
+		{
+			System.err.println("Excepción de E/S!!");
+			System.exit(-1);
+		}
+		catch (InterruptedException ex)
+		{
+			System.err.println("El proceso hijo finalizó de forma errónea");
+			System.exit(-1);
+		}
+	}
+}
---
+++ PSP_Topic1_Content1/src/RuntimeProcess/RuntimeProcess.java
@@ -0,0 +1,31 @@
+package RuntimeProcess;
+
+import java.io.IOException;
+import java.util.Arrays;
+public class RuntimeProcess
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		if (args.length <= 0)
+		{
+			System.err.println("Se necesita un programa a ejecutar");
+			System.exit(-1);
+		}
+		Runtime runtime = Runtime.getRuntime();
+		try
+		{
+			System.out.println("Ejecutando " + Arrays.toString(args));
+			// Otra forma de crear un proceso
+			Process process = runtime.exec(args);
+			// Esperar 3 segundos...
+			Thread.sleep(3000);
+			System.out.println("Destruyendo " + Arrays.toString(args));
+			process.destroy();
+		}
+		catch (IOException ex)
+		{
+			System.err.println("Excepción de E/S!!");
+			System.exit(-1);
+		}
+	}
+}
