ab0b83e6880f03e8bf5ab9017a9f41485bf333cc
Esteban Albarrán <77785532+Shylex96@users.noreply.github.com>
2023-10-25 09:24:02 +0200

    Add files via upload

---
+++ PSP_Topic2_Exercises1/src/exercise6/OpBasicas.java
@@ -0,0 +1,18 @@
+package exercise6;
+
+public class OpBasicas extends Thread {
+
+	int x, y;
+	OpBasicas(String nombre, int num1, int num2) {
+		super(nombre);
+		x = num1;
+		y = num2;
+	}
+	
+	public void run() {
+		System.out.println(x + " + " + y + " = " + (x+y) + " de hilo " + getName());
+		System.out.println(x + " - " + y + " = " + (x-y) + " de hilo " + getName());
+		System.out.println(x + " * " + y + " = " + (x*y) + " de hilo " + getName());
+		System.out.println(x + " / " + y + " = " + (x/y) + " de hilo " + getName());
+	}
+}
---
+++ PSP_Topic2_Exercises1/src/exercise7/Prueba.java
@@ -0,0 +1,29 @@
+package exercise7;
+
+import exercise6.OpBasicas;
+
+public class Prueba {
+
+	public static void main(String[] args)  throws InterruptedException {
+		
+		Storage m1 = new Storage("Múltiplos de 5", 5);
+		Storage m2 = new Storage("Múltiplos de 12", 12);
+		Storage m3 = new Storage("Múltiplos de 28", 28);
+		
+		Thread hilo1 = new Thread(m1);
+		Thread hilo2 = new Thread(m2);
+		Thread hilo3 = new Thread(m3);
+		
+		hilo1.start();
+		hilo2.start();
+		hilo3.start();
+		
+		hilo1.join();
+		hilo2.join();
+		hilo3.join();
+		
+		System.out.println("Hilos terminados");
+
+	}
+
+}
